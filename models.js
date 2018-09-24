542e3d91a3fd1e45328e67b3d36c52cfe6f427e1
Christian Mejia <chrismejia10@gmail.com>
2018-09-24 17:50:44 -0400

    Add working db associations and notes

--- models.js
+++ models.js
@@ -1,20 +1,52 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/plantr', {
-    logging: false
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/plantr", {
+  logging: false
 });
 
-const Gardener = db.define('gardeners', {
-    name: {
-        type: Sequelize.STRING,
-        // validation: {
-            // allowNull: false
-    },
-    age: {
-        type: Sequelize.INTEGER,
-        // validation: { 
-        //  allowNull: false   
-        }
-    }
-}
+// ### DB Definitions ###
+// Defining our tables, column names and corresponding data types
+const Gardener = db.define("gardeners", {
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER
+});
 
-module.exports = { db };
+const Plot = db.define("plots", {
+  size: Sequelize.INTEGER,
+  shaded: Sequelize.BOOLEAN
+});
+
+const Vegetable = db.define("vegetables", {
+  name: Sequelize.STRING,
+  color: Sequelize.STRING,
+  planted_on: Sequelize.DATE
+});
+
+// ### DB Relationships ###
+// Plot belongs to Gardener...
+Plot.belongsTo(Gardener);
+
+//...and a Gardener has one Plot.
+Gardener.hasOne(Plot);
+
+// Vegetables belongs to many plots.
+// Note 1. EVERY time you create a .belongsToMany relationship, you must specify an extra object containing a 'through' value that specifies the name to join table that contains the foreign keys.
+
+// A plot has many Vegetables; L36 & 37 defines the many-to-many relationship they have with each other. These two tables are connected { 'through' vegetable_plot }.
+//                         Note 1 ↴
+Vegetable.belongsToMany(Plot, { through: "vegetable_plot" });
+Plot.belongsToMany(Vegetable, { through: "vegetable_plot" });
+
+// This is handled on line 37 already!
+// Plot.hasMany(Vegetable);
+
+// A Gardener has a favorite vegetable.
+// Note 2: 'as' is just a universal alias
+
+// L46 is the one that breaks everything!? -- Nope, was L40; see notes on L34 & 39
+//                       Note 2 ↴
+Gardener.belongsTo(Vegetable, { as: "favorite_vegetable" });
+
+// ### Section ###
+
+// ### Exporting DB connection and DB tables ###
+module.exports = { db, Plot, Gardener, Vegetable };
--- seed.js
+++ seed.js
@@ -1,8 +1,7 @@
-const { db } = require('./models');
- 
-    db.sync({force: true})
-    .then((data => console.log('All synced up!')))
-    .catch(err =>
-    console.log('No dice Bryce'))
-    .finally((data => db.close()))
+const { db } = require("./models");
 
+// {force: true} will drop tables before every sync
+db.sync({ force: true })
+  .then(data => console.log("All synced up!"))
+  .catch(err => console.error("No dice Bryce", err))
+  .finally(data => db.close());
